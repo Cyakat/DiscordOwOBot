e55e92cf405286801d29b7007e3e3f01118394cb
Cyakat <36944093+Cyakat@users.noreply.github.com>
2019-04-30 13:05:59 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,50 +1,14 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt, content) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            case 'owo':
-                bot.sendMessage({
-                  to: channelID,
-                  message: 'UwU'
-                });
-            break;
-            case 'deleteMe':
-                bot.delete({
-                  to: channelID,
-                    
-                })
-            // Just add any case commands if you want to..
-         }
-     }
+bot.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
+
+bot.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+bot.login('token');
