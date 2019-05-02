887f946a3608993765cb8ca3cd935b148501bcf1
Cyakat <noah@thebauers.net>
2019-05-01 21:28:15 -0600

    added user adding and enabling / disabling of the owo func on channels

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-var channelIDs = [];
-var whitelist = ['249382933054357504']
+var aryChannelIDs = [];
+var aryWhitelist = ['249382933054357504', '1337'];
 var indexChan = 0;
 var indexWhite = 1;
 var prefix = '!';
@@ -18,6 +18,7 @@ bot.on('message', msg => {
   var user = msg.author.id;
   var username = msg.author.username;
   var channelID = msg.channel.id;
+  var userID;
 
   console.log(channelID)
 
@@ -26,19 +27,50 @@ bot.on('message', msg => {
   if (args[0] === prefix) {
     cmd = args.substr(1, args.length-1);
     switch (cmd) {
-      case 'add channel':
-        addChannel(user, channelID, msg)
+      case 'owo':
+        addChannel(user, channelID, msg, aryWhitelist)
         break;
-      case 'remove channel':
-        removeChannel(user, channelID, msg, channelIDs)
+      case 'uwu':
+        removeChannel(user, channelID, msg, aryChannelIDs, aryWhitelist)
         break;
     }
     args2 = cmd.split(' ');
+    if (args2[0] === 'add' && args2[1] === 'user') {
+      cmd = cmd.replace(/<@!/g, '');
+      cmd = cmd.replace(/<@/g, '');
+      cmd = cmd.replace(/>/g, '');
+      args2 = cmd.split(' ');
+      userID = args2[2];
+      console.log(userID);
+      addUser(user, userID, msg, aryWhitelist);
+    }
+    if (args2[0] === 'remove' && args2[1] === 'user') {
+      cmd = cmd.replace(/<@!/g, '');
+      cmd = cmd.replace(/<@/g, '');
+      cmd = cmd.replace(/>/g, '');
+      args2 = cmd.split(' ');
+      userID = args2[2];
+      removeUser(user, userID, msg, aryWhitelist);
+    }
+    /*args2 = cmd.split(' ');
+    args2 = args2.replace(/<@!/g, '');
+    args2 = args2.replace(/>/g, '');
+
+      switch (args2) {
+        case 'add user':
+          var userID = args2[2];
+          addUser(userID, msg);
+          break;
+        case 'remove user':
+          var userID = args2[2];
+          removeUser(userID, msg);
+          break;
+      }*/
 
   }
-  for (i; i<channelIDs.length; i++) {
-    if (channelID === channelIDs[i]) {
-      owo(user, msg, channelID, channelIDs[i]);
+  for (i; i<aryChannelIDs.length; i++) {
+    if (channelID === aryChannelIDs[i]) {
+      owo(user, msg, channelID, aryChannelIDs[i]);
     }
 }
 /*
@@ -107,50 +139,90 @@ function owo(user, msg, channelID, chanID) {
   }
 } */
 
-//function for adding channels to the whitelist
-function addChannel(userID, chanID, msg) {
-  if (userID === '249382933054357504') {
-    channelIDs[indexChan] = chanID;
-    indexChan = indexChan + 1;
-    msg.channel.send('IT HAS BEGUN')
-    console.log(`added channel ${chanID}`);
-  }else {
-    msg.channel.send("Someone's an idiot!");
+//function for adding channels to the aryWhitelist
+function addChannel(user, chanID, msg, aryWhitelist) {
+  for (i = 0; i < aryWhitelist.length; i++) {
+    if (user === aryWhitelist[i]) {
+      aryChannelIDs[indexChan] = chanID;
+      indexChan = indexChan + 1;
+      msg.channel.send('IT HAS BEGUN')
+      console.log(`added channel ${chanID}`);
+    }
   }
 }
 
-function removeChannel(userID, chanID, msg, channelIDs) {
-  if (userID ==='249382933054357504') {
-    for (i = 0; i < channelIDs.length; i++) {
-      if (chanID === channelIDs[i]) {
-        channelIDs[i] = '';
-        msg.channel.send("Y'ALL HAVE BEEN REMOVED FROM SLAVERY!");
-        console.log(`removed channel ${chanID}`);
+function removeChannel(user, chanID, msg, aryChannelIDs, aryWhitelist) {
+  for (i = 0; i < aryWhitelist.length; i++) {
+    if (user === aryWhitelist[i]) {
+      for (i = 0; i < aryChannelIDs.length; i++) {
+        if (chanID === aryChannelIDs[i]) {
+          aryChannelIDs[i] = '';
+          msg.channel.send("Y'ALL HAVE BEEN REMOVED FROM SLAVERY!");
+          console.log(`removed channel ${chanID}`);
+        }
       }
     }
-  }else {
-    msg.channel.send('YOU MAY NOT STOP THE TERROR!');
   }
 }
 
-//function for adding users to whitelist
-function addUser(userID, msg) {
-  if (userID === whitelist[indexWhite]) {
-    whitelist[indexWhite] = userID;
-    indexWhite = indexWhite + 1;
-    msg.channel.send('Added a user OwO!');
-  }
-}
-
-function removeUser(userID, msg) {
-  if (userID === whitelist[indexWhite])
-  for(i = 0; i < whitlist.length; i++) {
-    if (userID === whitelist[indexWhite]) {
-      whitelist[i] = '';
-      msg.channel.send('oof i guess a boi is no longer cool');
-      console.log(`removed a person ${userID}`);
+//function for adding users to aryWhitelist
+function addUser(user, userID, msg, aryWhitelist) {
+  var listComplete = false;
+  for (i = 0; i < aryWhitelist.length; i++) {
+   // msg.channel.send(i);
+    if (userID === aryWhitelist[i]){ /*aryWhitelist[i]*/ /*&& aryWhitelist[i] === '1337'){ /*|| userID === aryWhitelist[i-1]){*/ //&& aryWhitelist[i] === aryWhitelist[aryWhitelist.length-1]) {
+      if (user === userID) {
+        msg.channel.send("You can't add your self dummy!");
+        return;
+      } else {
+        console.log('user already added');
+        msg.channel.send('User already added siwwy UwU');
+        return;
+      }
     }
-  } else {
-    msg.channel.send('I guess louie sucks at coding...');
+    if (i === aryWhitelist.length-1) {
+      listComplete = true;
+    }
+    if (listComplete) {
+      for (i = 0; i < aryWhitelist.length; i++) {    // if (aryWhitelist[i] <= aryWhitelist.length)
+        if (user === aryWhitelist[i]) {
+          aryWhitelist[indexWhite] = userID;
+          aryWhitelist [indexWhite + 1] = '1337';
+          indexWhite = indexWhite + 1;
+          msg.channel.send('Added user ' + '<@' + userID + '>' + ' OwO!');
+          console.log(`added a user ${userID}`);
+          return;
+        }
+      }
+    }
+  }
+}
+
+function removeUser(user, userID, msg, aryWhitelist) {
+  var loopComplete = false;
+  for (i = 0; i < aryWhitelist.length; i++) {
+    if (userID === aryWhitelist[i]) {
+      loopComplete = true;
+      if (user !== userID ) {
+        for (i = 0; i < aryWhitelist.length; i++) {
+          if (user === aryWhitelist[i]) {
+            for( i = 0; i < aryWhitelist.length; i++) {
+              if (userID === aryWhitelist[i]) {
+                aryWhitelist[i] = ' ';
+                //indexWhite = indexWhite - 1;
+                msg.channel.send('oof I guess ' + '<@' + userID + '>' + ' is no longer cool ;w;');
+                console.log(`removed a person ${userID}`);
+              }
+            }
+          }
+        }
+      } else {
+        msg.channel.send("You wouldn't want to remove your self would you OwO");
+      }
+    }
+  }
+  if (i === aryWhitelist.length && loopComplete !== true) {
+    console.log('user was already removed');
+    msg.channel.send('The user is already gone you baka! (。-∀-)');
   }
 }
